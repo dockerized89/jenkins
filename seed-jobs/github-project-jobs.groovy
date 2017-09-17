8f23e99ff22102bcdb47878de035acc29416432d
Erik Johansson <erik@braindumps.io>
2017-09-17 20:14:07 +0200

    More small changes to seed job

--- seed-jobs/github-project-jobs.groovy
+++ seed-jobs/github-project-jobs.groovy
@@ -1,37 +1,37 @@
 def github_api = new URL("https://api.github.com/users/dockerized89/repos")
-def projects = new groovy.json.JsonSlurper().parse(github_api.newReader())
+def projects = new groovy.json.JsonSlurper().parse(githubApi.newReader())
 
 projects.each {
-	def job_name = it.name
-	def github_name = it.full_name
-	def github_url = it.ssh_url
-	println "Creating job ${job_name} for GitHub project ${github_url}"
+  def job_name=it.name
+  def github_name=it.full_name
+  def git_url=it.ssh_url
+  println "Creating Job ${job_name} for ${git_url}"
 
-	job("Github-${job_name}") {
-		logRotator(-1, 10)
-		scm {
-			github(github_name, 'master')
-		}
-		trigger {
-			githubPush()
-		}
-	}
+  job("GitHub-${job_name}") {
+    logRotator(-1, 10)
+    scm {
+        github(github_name, 'master')
+    }
+    triggers {
+        githubPush()
+    }
+  }
 }
 
-listView('My Github Jobs') {
-	description('Seed job for setting up all my Github projects as jobs in Jenkins')
-	filterBuildQueue()
-	filterExecutors()
-	jobs {
-		regex(/Github-.*/)
-	}
-	columns {
-		status()
-		buildButton()
-		weather()
-		name()
-		lastSuccess()
-		lastFailure()
-		lastDuration()
-	}
+listView('My GitHub Jobs') {
+    description('')
+    filterBuildQueue()
+    filterExecutors()
+    jobs {
+        regex(/GitHub-.*/)
+    }
+    columns {
+        status()
+        buildButton()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+    }
 }
